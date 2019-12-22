2de5f1ccc4f93525eb3be41ba322477ee0904c84
Loïs B <abeetlott@gmail.com>
2019-12-22 16:49:55 +0100

    Added "Tickable.Stop" method test

--- TheTicker_UnityProject/Assets/Scripts/ExampleGameManager.cs
+++ TheTicker_UnityProject/Assets/Scripts/ExampleGameManager.cs
@@ -13,15 +13,26 @@ public class ExampleGameManager : MonoBehaviour
     private Ticker m_ticker; 
     public Ticker Ticker { get { return m_ticker; } }
 
+    private ExampleTickable m_tickable1;
+    private ExampleTickable m_tickable2;
+
     //let's instantiate some Tickables for testing purpose
     void Start()
     {
-        ExampleTickable tickable1 = new ExampleTickable(m_ticker, 0.5f);    //ticks twice a second
-        tickable1.m_name = "ExampleTickable 1";
-        ExampleTickable tickable2 = new ExampleTickable(m_ticker, 1f);      //ticks once a second
-        tickable2.m_name = "ExampleTickable 2";
+        m_tickable1 = new ExampleTickable(m_ticker, 0.5f);    //ticks twice a second
+        m_tickable1.m_name = "ExampleTickable 1";
+        m_tickable2 = new ExampleTickable(m_ticker, 1f);      //ticks once a second
+        m_tickable2.m_name = "ExampleTickable 2";
 
-        tickable1.Start();
-        tickable2.Start();
+        m_tickable1.Start();
+        m_tickable2.Start();
+
+        StartCoroutine(StopTickableAfterSomeTimeTest(m_tickable1, 3f));
+    }
+
+    private IEnumerator StopTickableAfterSomeTimeTest(Tickable _tickable, float _time)
+    {
+        yield return new WaitForSeconds(_time);
+        _tickable.Stop();
     }
 }
